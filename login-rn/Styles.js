08adfa668937d0a469ef5424e9ce6fa98d8e31a9
user <surachtsungkhapan@gmail.com>
2023-09-22 10:45:59 +0700

    new login

---
+++ login-rn/Styles.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Styles = () => {
+  return (
+    <View>
+      <Text>Styles</Text>
+    </View>
+  )
+}
+
+export default Styles
